c2cf82db1ae7c364fde1edc98985a1909fc3accd
AZ <41629560+AFTM7512@users.noreply.github.com>
2020-06-28 17:06:21 +0800

    add-增加多选功能 (#1)
    * add-开始增加多选功能
    
    * fix-增加多选
    
    * fix-增加 maxTagCount
    
    * fix-完成多选功能
    
    * fix-完成 readme文档
    
    * fix-修改样式
    
    * fix-初步完成
    
    * fix-初步完成
    
    Co-authored-by: AFTM7512 <jemthods@163.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <tree width="240px" :dataSource="treeData" @change="handleChange" />
+    <p>
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </p>
+    <tree
+      style="margin: 0 auto"
+      mode
+      :dataSource="treeData"
+      :maxTagCount="2"
+      :defaltMulValue="[2, 4, 5]"
+      :mulValue="mulValue"
+      @change="handleChange"
+    />
   </div>
 </template>
 
@@ -18,62 +28,53 @@ export default {
   data() {
     return {
       treeData: [
-        // {
-        //   label: '总部1',
-        //   id: 1,
-        //   type: 'organization',
-        //   children: [
-        //     {
-        //       label: '产品1',
-        //       id: 2,
-        //       type: 'department',
-        //     },
-        //     {
-        //       label: '技术1',
-        //       id: 3,
-        //       type: 'organization',
-        //       children: [
-        //         {
-        //           label: '大前端组',
-        //           id: 4,
-        //           type: 'department',
-        //         },
-        //         {
-        //           label: '后台组',
-        //           id: 5,
-        //           type: 'department',
-        //         },
-        //         {
-        //           label: '运维组',
-        //           id: 6,
-        //           type: 'department',
-        //         },
-        //       ],
-        //     },
-        //     {
-        //       label: '销售',
-        //       id: 7,
-        //       type: 'department',
-        //     },
-        //     {
-        //       label: '人事',
-        //       id: 8,
-        //       type: 'department',
-        //     },
-        //     {
-        //       label: '财务',
-        //       id: 9,
-        //       type: 'department',
-        //     },
-        //   ],
-        // },
+        {
+          label: '总部1',
+          id: 1,
+          type: 'organization',
+          children: [
+            {
+              label: '产品1',
+              id: 2,
+              type: 'department',
+            },
+            {
+              label: '技术1',
+              id: 3,
+              type: 'organization',
+              children: [
+                {
+                  label: '大前端组',
+                  id: 4,
+                  type: 'department',
+                },
+                {
+                  label: '后台组',
+                  id: 5,
+                  type: 'department',
+                },
+                {
+                  label: '运维组',
+                  id: 6,
+                  type: 'department',
+                },
+              ],
+            },
+            {
+              label: '销售',
+              id: 7,
+              type: 'department',
+            },
+          ],
+        },
       ],
+      mulValue: [],
     }
   },
 
   methods: {
-    handleChange() {
-
+    handleChange(val) {
+      this.mulValue = val
     }
   }
 };
